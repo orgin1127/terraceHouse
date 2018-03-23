691b29fc7b25b87bb91a6f7ee1f6cbfa06cf2afc
Origin <kwanhun21@gmail.com>
2018-03-23 13:12:39 +0900

    commit

--- src/main/java/com/SpringBoot/Demo/WebRestController/WebController.java
+++ src/main/java/com/SpringBoot/Demo/WebRestController/WebController.java
@@ -1,7 +1,10 @@
 package com.SpringBoot.Demo.WebRestController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.SpringBoot.Demo.Service.PostService;
 
 import lombok.AllArgsConstructor;
 
@@ -9,8 +12,10 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class WebController {
 	
+	private PostService postService;
 	@GetMapping("/")
-	public String main() {
+	public String main(Model model) {
+		model.addAttribute("posts", postService.findAllDesc());
 		return "main";
 	}
 }
