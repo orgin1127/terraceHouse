3372ed7a903442b4030dcdda560ac30197ba2ae4
Origin <kwanhun21@gmail.com>
2018-04-03 18:16:55 +0900

    jsp test 5
    plz

--- src/main/java/com/SpringBoot/Demo/WebRestController/WebController.java
+++ src/main/java/com/SpringBoot/Demo/WebRestController/WebController.java
@@ -4,16 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.SpringBoot.Demo.Service.PostService;
 
 import lombok.AllArgsConstructor;
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class WebController {
 	
 	private PostService postService;
+	
 	@GetMapping("/")
 	public String main(Model model) {
 		model.addAttribute("posts", postService.findAllDesc());
