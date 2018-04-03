a7dbadf60382d71c533170c7f3bf1859bced4ddc
Origin <kwanhun21@gmail.com>
2018-04-03 19:06:02 +0900

    jsp test 12
    commit

--- src/main/java/com/SpringBoot/Demo/SpringBootDemoApplication.java
+++ src/main/java/com/SpringBoot/Demo/SpringBootDemoApplication.java
@@ -4,35 +4,36 @@ package com.SpringBoot.Demo;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@ComponentScan
-@Configuration
+
 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class SpringBootDemoApplication extends SpringBootServletInitializer{
 	
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+"classpath:application.yml,"
 			+"/app/config/terraceHouse/real-application.yml";
+	
+	
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringBootDemoApplication.class);
+	}
+
+
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(SpringBootDemoApplication.class)
 		.properties(APPLICATION_LOCATIONS)
 		.run(args);
 	}
-	
-	@Bean
-    public InternalResourceViewResolver setupViewResolver() {
- 
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
- 
-        resolver.setPrefix("/WEB-INF/jsp/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
+
 }
