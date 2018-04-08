6cad7967cc27d2f7556ecba2577ef90bb010f6a2
Origin <kwanhun21@gmail.com>
2018-04-08 19:59:04 +0900

    delete

--- src/main/java/com/SpringBoot/Demo/WebRestController/WebController.java
+++ src/main/java/com/SpringBoot/Demo/WebRestController/WebController.java
@@ -3,10 +3,7 @@ package com.SpringBoot.Demo.WebRestController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.SpringBoot.Demo.Service.PostService;
 
 import lombok.AllArgsConstructor;
 
--- src/main/java/com/SpringBoot/Demo/WebRestController/WebRestController.java
+++ src/main/java/com/SpringBoot/Demo/WebRestController/WebRestController.java
@@ -2,19 +2,10 @@ package com.SpringBoot.Demo.WebRestController;
 
 import java.util.Arrays;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.SpringBoot.Demo.DTO.Posts.PostsSaveRequestDTO;
-import com.SpringBoot.Demo.Domain.PostsRepository;
-import com.SpringBoot.Demo.Service.PostService;
 
 import lombok.AllArgsConstructor;
 
@@ -24,7 +15,6 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class WebRestController {
 	
-	private PostService postService;
 	private Environment environment;
 
 	@GetMapping("/hello")
@@ -32,10 +22,6 @@ public class WebRestController {
 		return "Hello World! 그리고 안녕";
 	}
 	
-	/*@PostMapping("/posts")
-	public Long savePosts(@RequestBody PostsSaveRequestDTO dto){
-		return postService.Save(dto);
-	}*/
 	
 	@GetMapping("/profile")
 	public String getProfile() {
