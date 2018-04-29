2b10f3004b501b59c97abb4c1f043972c4c50899
Origin <kwanhun21@gmail.com>
2018-04-29 15:34:46 +0900

    3:34
    test

--- src/main/java/com/SpringBoot/Demo/dto/RegularTerraceMemberSaveRequestDto.java
+++ src/main/java/com/SpringBoot/Demo/dto/RegularTerraceMemberSaveRequestDto.java
@@ -1,7 +1,42 @@
 package com.SpringBoot.Demo.dto;
 
+import com.SpringBoot.Demo.Domain.Member.Member;
+import com.SpringBoot.Demo.Domain.RegularTerrace.RegularTerrace;
+import com.SpringBoot.Demo.Domain.RegularTerraceMember.RegularTerraceMember;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegularTerraceMemberSaveRequestDto {
+	
+	private Long regular_terrace_member_number;
+	private RegularTerrace regularTerrace;
+	private Member member;
+	
+	@Builder
+	public RegularTerraceMemberSaveRequestDto(RegularTerrace regularTerrace, Member member){
+		this.regularTerrace = regularTerrace;
+		this.member = member;
+	}
+	
+	public RegularTerraceMember toEntity(){
+		return RegularTerraceMember.builder()
+									.regularTerrace(regularTerrace)
+									.member(member)
+									.build();
+	}
 
+	@Override
+	public String toString() {
+		return "RegularTerraceMemberSaveRequestDto [regular_terrace_member_number=" + regular_terrace_member_number
+				+ ", regularTerrace=" + regularTerrace + ", member=" + member + "]";
+	}
+	
 }
