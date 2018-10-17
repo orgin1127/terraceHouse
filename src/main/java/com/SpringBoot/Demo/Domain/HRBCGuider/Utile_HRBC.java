6edb424528bffe4eb82e9966107e17e493f8f6d3
Chihyung Park <chihyung.park@porters.jp>
2018-10-17 20:28:34 +0900

    2018.10.17_ chi
    Util_HRBC
    field and option part modification

--- src/main/java/com/SpringBoot/Demo/Domain/HRBCGuider/Utile_HRBC.java
+++ src/main/java/com/SpringBoot/Demo/Domain/HRBCGuider/Utile_HRBC.java
@@ -11,32 +11,109 @@ import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public class Utile_HRBC {
 
 	// Hrbc 전체 필드 가져오기 
-	public ArrayList<HRBCField> getField(String classification){ // classification is job or resume etc
+	public ArrayList<HRBCField> getField(String classification, String token){ // classification is job or resume etc
 		
 		ArrayList<HRBCField> fieldList = new ArrayList<HRBCField>();
 		
+		String url = "http://hrbc1-api.localvm/v1/field?partition=1421&resource=3&count=200&active=-1&start=0";
 		
+		Connection.Response response;
+		
+		try {
+		
+			Document document =Jsoup.connect(url)
+					.header("Content-Type","application/xml; charset=UTF-8")
+			        .header("X-porters-hrbc-oauth-token", token)
+			        .parser(Parser.xmlParser()).timeout(3000).get();
+					
+		Elements rawData = document.getElementsByTag("item");
+		
+		for(Element e:rawData){
+			
+			String name = e.getElementsByTag("field.p_name").text();
+			//int dataType = Integer.parseInt(e.getElementsByTag("field.p_type").text());
+			String dataType = e.getElementsByTag("field.p_type").text();
+			
+			HRBCField field = new HRBCField();
+			
+			field.setDataType(dataType);
+			field.setFieldName(name);
+			
+			fieldList.add(field);	
+		}
+		
+		for(HRBCField a: fieldList){
+		
+			System.out.println("fieldData: "+ a);
+		}
+		
+				
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return fieldList;
 	}
 	
 	// Hrbc 전체 옵션 및 옵션벨류 가져오기 
-	public ArrayList<HRBCOption> getOption(){
+	public ArrayList<HRBCOption> getOption(String token){
 		
 		ArrayList<HRBCOption> optionList = new ArrayList<HRBCOption>();
 		
+		String url = "http://hrbc1-api.localvm/v1/option?partition=1421&level=1&enabled=-1";
+	
+		try {
 		
-		//
+			Document document =Jsoup.connect(url)
+					.header("Content-Type","application/xml; charset=UTF-8")
+			        .header("X-porters-hrbc-oauth-token", token)
+			        .parser(Parser.xmlParser()).timeout(3000).get();
+		
+		System.out.println(document.html());
+					
+		Elements rawData = document.getElementsByTag("item");
+		
+		Elements rawData1 = rawData.parents();
+		
+		Elements rawData2 = document.children();// 바로 밑의 칠드런 
+		
+		for(Element e:rawData){
+			
+			String name = e.getElementsByTag("option.p_name").text();
+			
+			//Element e_under = e.getElementById("item");
+			
+			HRBCOption option = new HRBCOption();
+			
+			option.setOptionName(name);
+			
+			optionList.add(option);	
+		}
+		
+		
+		for(HRBCOption a: optionList){
+		
+			System.out.println("OptionData: "+ a.getOptionName());
+		}	
+		
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return optionList;
 	}
 	
 	
 	// Hrbc 엘리어스에 해당되는 옵션    을  사용하는 필드 값 찾기 
-	public String findField(String optionAias){
+	public String findField(String optionAias, String token){
 		
 		
 		return "";
@@ -44,80 +121,12 @@ public class Utile_HRBC {
 	
 	
 	// 필드에서 사용하고 있는 옵션 선택지 가져오기
-	public ArrayList<String> getOptionList(String fieldName){
+	public ArrayList<String> getOptionList(String fieldName, String token){
 		
 		ArrayList<String> optionList = new ArrayList<String>();
 		
 		
 		return optionList; 
 	}
-	
-public String connectOptionData(String url, String token){
-		
-		Connection.Response response;
-		String btnk= "not entered";
-		
-		try {
-
-			/*			response = ((Connection) Jsoup.connect(url)
-			        .method(Connection.Method.GET)
-			        .header("X-porters-hrbc-oauth-token", token)
-			        .parser(Parser.xmlParser()).timeout(3000).get())
-
-
-			        //.header("Content-Type","application/xml; charset=UTF-8")
-			        .header("Access-Control-Allow-Origin" , "*")
-			        .header("Access-Control-Allow-Methods" , "GET, POST, OPTIONS")
-			        .header("Access-Control-Max-Age" , "3600")
-			        .header("Access-Control-Allow-Headers" , "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization")
-			        
-			        .execute();
-		
-		Document document = response.parse();
-		
-*/		
-		
-			Document document =Jsoup.connect(url)
-					.header("Content-Type","application/xml; charset=UTF-8")
-			        .header("X-porters-hrbc-oauth-token", token)
-			        .parser(Parser.xmlParser()).timeout(3000).get() 
-					;
-		
-		
-		/*String str  = document.html();
-		
-		System.out.println(str);*/
-		
-		Elements el  = document.getElementsByTag("option.p_name");
-		
-		ArrayList<String> optionList = new ArrayList<String>();
-		
-		for(Element e: el){
-			
-		String a = e.text();
-			
-		optionList.add(a);	
-			
-		}
-		
-		System.out.println("---------------------------------Optiondata check ----------------------------------");
-		
-		for(String a: optionList){
-		System.out.println("optionData: "+ a);
-		}
-		
-		//Element codeElement = googleDocument.select("Code").first();
-		
-		//btnk = codeElement.text();
-		
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return btnk;
-	}
-	
-	
 	
 }
