5ff643abc84e00c91e27ada2f22993df41884cf1
Sojeong Park <qtw0626@gmail.com>
2018-10-18 13:29:05 +0900

    181018 *.css changed
    colour unified (#589B32)
    Submit&Next button css changed
    div boder-rounded
    set td width rate 20% each
    margin/padding value detail modifed

--- src/main/resources/static/js/app/customHelper.js
+++ src/main/resources/static/js/app/customHelper.js
@@ -5,22 +5,78 @@
 function uploadExcel() {
 	var form = document.getElementById('excelUpload');
 	var formData = new FormData(form);
-	
+	var itemsList = [];
+	var dataList = [];
 	$.ajax({
 		url: 'uploadExcel',
 		type: 'post',
 		enctype: 'multipart/form-data',
         data: formData,
-        contentType: "application/x-www-form-urlencoded; charset=UTF-8", 
+        dataType: 'json',
+        //contentType:'application/json; charset=utf-8',
         processData: false, //prevent jQuery from automatically transforming the data into a query string
         contentType: false,
         cache: false,
         success: function(result) {
-			if (result == 1){
-				console.log('success');
-			}
+        	
+        	console.log("success");
+        	
+        	itemList = result.itemList;
+        	dataList = result.dataList;
+        	
+        	//console.log(JSON.stringify(result.itemList));
+        	
+        	var str ='<form action="itemChange" method="post">'; 
+        		str+= '<table class = "type05">';
+			$.each(itemList, function(index,values) {
+				console.log("항목 : " + values);
+				str+='<tr> <td class="itemcheckboxes" style="margin-top: 0px;margin-left: 0px;margin-right: 0px;width: 45px; padding-top: 8px; padding-bottom: 8px; padding-left: 5px; padding-right: 5px; background-color: #f1f4f7;">';
+				str+='<label class="container">';
+				str+='<input type="checkbox" checked="checked" id="item'+index+'" value="'+values+'" name="excelItems">';	
+				str+='<span class="checkmark"></span></label></td><td>';
+				str+=values;
+				str+='</td></tr>';
+			});
+			str+='</table>';
+			str+='<div class = "rightFloat">';
+			str+='<input type="button" class="buttonCss" id="itemSelectBtn" value="Submit"></form>';
+			str+='</div>';
+			
+			$('#itemListCheckArea').html(str);
+			
+			$('#itemSelectBtn').on('click', confirmCheck);
+			
+			$.each(dataList, function(index,values) {
+				console.log("데이터 : " + values);
+			});
 		},
 		error: function (e) {
+			console.log(e);
+		}
+	});
+}
+
+function confirmCheck() {
+	var checkedItemList = [];
+	
+
+	$('input:checkbox[name="excelItems"]').each(function() {
+		if ($(this).is(":checked") == true) {
+			checkedItemList.push($(this).val());
+		}
+	});
+	console.log(checkedItemList.length);
+	
+	$.ajax({
+		url: 'confirmCheck',
+		type: 'post',
+		data: {'checkedItemList' : checkedItemList},
+		dataType: 'json',
+		traditional: true,
+		success: function(result) {
+			console.log(result);
+		},
+		error: function(e) {
 			console.log(e);
 		}
 	});
